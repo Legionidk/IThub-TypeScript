dd404ac949be71de644745ec132753b6c670a923
Legionidk <legionidk@internet.ru>
2026-05-13 14:38:05 +0300

    updated main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,24 @@
-import Warrior from "./baseCharacters/warrior";
-import Wizard from "./baseCharacters/wizard";
-import Archer from "./baseCharacters/archer";
+import FireFactory from "./factories/fireFactory";
+import FrostFactory from "./factories/frostFactory";
+import PoisonFactory from "./factories/poisonFactory";
 
-import Fire from "./specialCharacters/fire";
-import Frost from "./specialCharacters/frost";
-import Poison from "./specialCharacters/poison";
-
-const baseWarrior = new Warrior();
-const baseWizard = new Wizard();
-const baseArcher = new Archer();
+const fireFactory = new FireFactory();
+const frostFactory = new FrostFactory();
+const poisonFactory = new PoisonFactory();
 
 const army = [
-    [new Fire(baseWarrior), new Fire(baseWizard), new Fire(baseArcher)],
-    [new Frost(baseWarrior), new Frost(baseWizard), new Frost(baseArcher)],
-    [new Poison(baseWarrior), new Poison(baseWizard), new Poison(baseArcher)],
+    [fireFactory.archer(), fireFactory.warrior(), fireFactory.wizard()],
+    [frostFactory.archer(), frostFactory.warrior(), frostFactory.wizard()],
+    [poisonFactory.archer(), poisonFactory.warrior(), poisonFactory.wizard()],
 ];
+
+console.log(fireFactory.archer());
 
 for (const specialArmy of army) {
     for (const specialUnit of specialArmy) {
         console.log(specialUnit.name);
         console.log(`Базовый урон: ${specialUnit.baseDamage}`);
-        console.log(`Способность: ${specialUnit.ability}`)
-        console.log()
+        console.log(`Способность: ${specialUnit.ability}`);
+        console.log();
     }
 }
